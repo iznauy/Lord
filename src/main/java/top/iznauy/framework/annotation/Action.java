01bc7e39bc8f1c20550f58cb94230a9e84cc2600
yuan <zy05160516@126.com>
2019-04-13 00:00:29 +0800

    add some annotations

---
+++ src/main/java/top/iznauy/framework/annotation/Action.java
@@ -0,0 +1,11 @@
+package top.iznauy.framework.annotation;
+
+/**
+ * Created on 12/04/2019.
+ * Description:
+ *
+ * @author iznauy
+ */
+
+public @interface Action {
+}
---
+++ src/main/java/top/iznauy/framework/core/BeanFactory.java
@@ -0,0 +1,16 @@
+package top.iznauy.framework.core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created on 12/04/2019.
+ * Description:
+ *
+ * @author iznauy
+ */
+public class BeanFactory {
+
+    private static final Map<Class<?>, Object> BEAN_MAP = new HashMap<>();
+
+}
